b57defe6f1ebff5ac88c2ebaef76aceb93651797
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-17 20:25:54 -0300

    components, service and models

---
+++ src/app/models/TarjetaCredito.ts
@@ -0,0 +1,13 @@
+export class TarjetaCredito {
+    id?: string;
+    titular: string;
+    numeroTarjeta: string;
+    fechaExpiracion: string;
+    cvv: number;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+
+    constructor(titular: string, numeroTarjeta: string, fechaExpiracion: string, cvv: number){
+        
+    }
+}
