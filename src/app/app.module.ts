49d9992b25678f75090d91ee8ee4ca1571099b97
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-20 20:17:50 -0300

    configuring firestore in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppComponent } from './app.component';
 import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
 import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarje
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDYaXKkVVmpjQN7O2d-GH0pRmH7fx7TIlA",
+    authDomain: "tarjetacreditoangular-5ed9c.firebaseapp.com",
+    projectId: "tarjetacreditoangular-5ed9c",
+    storageBucket: "tarjetacreditoangular-5ed9c.appspot.com",
+    messagingSenderId: "796708376577",
+    appId: "1:796708376577:web:87cdb9eef6c8c4bdf8b000"
+  }
 };
