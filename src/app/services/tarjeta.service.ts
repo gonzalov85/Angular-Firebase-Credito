b1e11b79f448e6d09c456e2bc7ae13427cfbd7be
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-20 20:32:04 -0300

    save point injected module into tarjeta service

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { TarjetaCredito } from '../models/TarjetaCredito';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaService {
 
-  constructor() { }
+  constructor(private firebase: AngularFirestore) { }
+
+  guardarTarjeta(tarjeta: TarjetaCredito){
+    this.firebase.collection('tarjetas').add(tarjeta);
+  }
 }
