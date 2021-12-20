7abbfcccc76d5530361c3f17ab19cfd22c559000
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-20 20:13:02 -0300

    installed firebase cli and configured it

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDYaXKkVVmpjQN7O2d-GH0pRmH7fx7TIlA",
+    authDomain: "tarjetacreditoangular-5ed9c.firebaseapp.com",
+    projectId: "tarjetacreditoangular-5ed9c",
+    storageBucket: "tarjetacreditoangular-5ed9c.appspot.com",
+    messagingSenderId: "796708376577",
+    appId: "1:796708376577:web:87cdb9eef6c8c4bdf8b000"
+  }
 };
 
 /*
