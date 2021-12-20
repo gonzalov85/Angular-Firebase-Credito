2842e2a6f27db8e8d3c2ddeda85bd8851eb3d35a
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-20 18:41:42 -0300

    adding form group module and interactions

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearTarjetaComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', Validators.required],
+      fechaExpiracion: ['', Validators.required],
+      cvv: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
